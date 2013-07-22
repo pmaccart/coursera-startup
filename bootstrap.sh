9fa3a7d745bbed33c82103ddcc3c71a2b6e1897e
philm <phil.maccart@slalom.com>
2013-07-21 19:18:54 -0500

    updates to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,12 @@ sudo apt-get install -y git-core
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku login
 ssh-keygen -t rsa
-heroku keys:addi
+heroku keys:add
 
+git config --global user.name pmaccart
+
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
